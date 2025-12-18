87feda0a973f8427db94ea35d1e3ef5a31e7e3df
daangamarca2 <arturoangamarca120@gmail.com>
2025-12-18 15:54:05 -0500

    Problema-06

--- Taller10/src/paquete6/Problema06.java
+++ Taller10/src/paquete6/Problema06.java
@@ -5,10 +5,52 @@
  */
 package paquete6;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema06 {
 
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        
+        int[][] matriz = new int[5][6];
+        int[][] matrizResultante = new int[5][6];
+        int valor;
+
+        System.out.print("Ingrese el valor entero: ");
+        valor = entrada.nextInt();
+
+        for (int fila = 0; fila < matriz.length; fila++) {
+            for (int columna = 0; columna < matriz[fila].length; columna++) {
+                System.out.printf("Ingrese valor para [%d][%d]: ", fila, columna);
+                matriz[fila][columna] = entrada.nextInt();
+            }
+        }
+
+        for (int fila = 0; fila < matriz.length; fila++) {
+            for (int columna = 0; columna < matriz[fila].length; columna++) {
+                matrizResultante[fila][columna] = matriz[fila][columna] * valor;
+            }
+        }
+        System.out.println("\n\t--- Matriz ---");
+        for (int fila = 0; fila < matriz.length; fila++) {
+            for (int columna = 0; columna < matriz[fila].length; columna++) {
+                System.out.printf("%4d ", matriz[fila][columna]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n  --- Matriz Resultante ---");
+        for (int fila = 0; fila < matrizResultante.length; fila++) {
+            for (int columna = 0; columna < matrizResultante[fila].length; columna++) {
+                System.out.printf("%4d ", matrizResultante[fila][columna]);
+            }
+            System.out.println();
+        }
+    }
 }
