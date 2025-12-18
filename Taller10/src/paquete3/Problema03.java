e1cdcbae37adfaf081dbeedd7c20e718d684965e
daangamarca2 <arturoangamarca120@gmail.com>
2025-12-18 15:35:45 -0500

    Problema-03

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,5 +10,45 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
-    
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        boolean[][] sensores = {
+            {true, true, true, true, true, true},
+            {true, false, true, false, true, false},
+            {false, false, false, false, false, false},
+            {true, true, false, true, true, false}};
+        String mensaje = "";
+        int[] funcional = new int[sensores.length];
+        int[] noFuncional = new int[sensores.length];
+        
+        for (int fila = 0; fila < sensores.length; fila++) {
+            int contadorFuncional = 0;
+            int contadorNoFuncional = 0;
+            for (int columna = 0; columna < sensores[fila].length; columna++) {
+                if (sensores[fila][columna] == true) {
+                    contadorFuncional = contadorFuncional + 1;
+                } else {
+                    contadorNoFuncional = contadorNoFuncional + 1;
+                }
+            }
+            funcional[fila] = contadorFuncional;
+            noFuncional[fila] = contadorNoFuncional;
+        }
+        int contador = 1;
+        for (int i = 0; i < 4; i++) {
+
+            if (funcional[i] == 6) {
+                mensaje = mensaje + String.format("Zona %d: SIN FALLOS\n",
+                        contador);
+            } else {
+                mensaje = mensaje + String.format("Zona %d: %d FALLOS\n",
+                        contador, noFuncional[i]);
+            }
+            contador = contador + 1;
+        }
+        System.out.println("REPORTE DE LOS SENSORES\n");
+        System.out.printf(mensaje);
+    }
 }
--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -5,10 +5,62 @@
  */
 package paquete4;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema04 {
-    
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        int filas = 3, columnas = 2;
+        int[][] matriz1 = new int[filas][columnas];
+        int[][] matriz2 = new int[filas][columnas];
+        boolean mayorIgual = true;
+        boolean mayor = false;
+
+        System.out.printf("Ingrese los valores para las matrices A y B (%dx%d):"
+                + "\n", filas, columnas);
+        
+        for (int indice = 0; indice < filas; indice++) {
+            for (int subIndice = 0; subIndice < columnas; subIndice++) {
+                System.out.printf("A[%d][%d]: ", indice, subIndice);
+                matriz1[indice][subIndice] = entrada.nextInt();
+                System.out.printf("B[%d][%d]: ", indice, subIndice);
+                matriz2[indice][subIndice] = entrada.nextInt();
+            }
+        }
+        for (int f = 0; f < filas; f++) {
+            for (int c = 0; c < columnas; c++) {
+                System.out.printf("\nMATRIZ A:\n%d\t", matriz1[f][c]);
+            }
+        }
+
+        for (int f = 0; f < filas; f++) {
+            for (int c = 0; c < columnas; c++) {
+                System.out.printf("\nMATRIZ B:\n%d\t", matriz2[f][c]);
+            }
+        }
+        for (int f = 0; f < filas; f++) {
+            for (int c = 0; c < columnas; c++) {
+                if (matriz1[f][c] < matriz2[f][c]) {
+                    mayorIgual = false;
+                } else if (matriz1[f][c] > matriz2[f][c]) {
+                    mayor = true;
+                }
+            }
+        }
+        if (mayorIgual && mayor) {
+            System.out.println("\n\nLa matriz A es mayor que la matriz B\n");
+        } else {
+            System.out.println("\n\nLa matriz A no es mayor que la matriz B\n");
+        }
+    }
 }
